6e9d0b476936310f578cdccc7a9ed2dbaa47fec7
Kieran King <kieranking3555@gmail.com>
2022-03-24 17:12:39 -0700

    Added Assignment 5 requirement user stories

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Kieran King on 3/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
